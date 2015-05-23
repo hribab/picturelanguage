ef57c46517c7447cda9955ccdcb8b790090c1365
hari babu <itzhari.g@gmail.com>
2015-05-22 22:43:04 -0700

    keyword with images

--- getresults.php
+++ getresults.php
@@ -1,8 +1,38 @@
 <?php 
 
+
+
+$text="Gaffigan was also one of the first comedians to sell his specials online, and he sees the Internet as a great place to get noticed in comedy. While it helps that he has 5 million or so Twitter followers, he wants to see comedians, performers, and producers embrace the Internet and not fear it… a tall order from an industry that still doesn’t know what the make of a great comedian giving away his first show for free.";
+
+include "module/AlchemyAPI.php";
+
+
+// Create an AlchemyAPI object.
+$alchemyObj = new AlchemyAPI();
+
+// Load the API key from disk.
+$alchemyObj->loadAPIKey("api_key.txt");
+
+
+//$alcEntities = @$alchemyObj->TextGetRankedNamedEntities($text,'json', $namedEntityParams) ;
+//$alcRelations = @$alchemyObj->TextGetRelations($text,'json');
+$alcKeyowrds = @$alchemyObj->TextGetRankedKeywords($text, 'json');
+
+$alckeywordarr = json_decode($alcKeyowrds, true); 
+
+
+for($i=0;$i<4;$i++)
+{
+echo $alckeywordarr["keywords"][$i]["text"]."\n";
+}
+/*
 $r=json_decode(file_get_contents("res.json"),'ture');
 
 for($i=0;$i<4;$i++){
 echo $r["d"]["results"][$i]["MediaUrl"]."\n";
 
 }
+
+*/
+
+?>
--- index.php
+++ index.php
@@ -39,11 +39,15 @@ $namedEntityParams = new AlchemyAPI_NamedEntityParams();
 $namedEntityParams->setSentiment(1);
 $alcEntities = @$alchemyObj->TextGetRankedNamedEntities($text,'json', $namedEntityParams) ;
 $alcRelations = @$alchemyObj->TextGetRelations($text,'json');
+$alcKeyowrds = @$alchemyObj->TextGetRankedKeywords($text, 'json');
+	
 	
 $alcEntityArr = json_decode($alcEntities, true); 
 $alcResponseArr = json_decode($alcRelations, true);
+$alckeywordarr = json_decode($alcKeyowrds, true); 
+	
 
-$ab=array_merge($alcEntityArr,$alcResponseArr);
+$ab=array_merge($alcEntityArr,$alcResponseArr, $alckeywordarr);
 $collection->insert($ab);
 	
 echo "<h1>People</h1></br>";
@@ -75,6 +79,7 @@ echo "<h1>People</h1></br>";
 			echo "<img src='".$response["d"]["results"][$i]["MediaUrl"]."' width='200' height='200'>";
 		}
 		}
+		echo "</br>";
 	}
 echo "<h1>Company</h1></br>";
 
@@ -105,6 +110,7 @@ echo "<h1>Company</h1></br>";
 			echo "<img src='".$response["d"]["results"][$i]["MediaUrl"]."' width='200' height='200' >";
 		}
 	}
+	echo "</br>";
 	}	 
 		
 echo "<h1>Other Entities</h1></br>";
@@ -138,6 +144,42 @@ if ($alcResponseArr['status'] == 'OK')
 					}
 		echo "</br></br>";
 	}
-	}	
 	}
+
+echo "</br></br></br>";
+echo "<h1>Keywords</h1></br>";
+
+	for($i=0;$i<5;$i++)
+	{
+	echo $alckeywordarr["keywords"][$i]["text"]."</br>";
+	$query = $alckeywordarr["keywords"][$i]["text"];
+		$serviceOp ='Image';
+		$market ='en-us';
+		$query = urlencode("'$query'");
+		$market = urlencode("'$market'");
+		$requestUri = "$rootUri/$serviceOp?\$format=json&Query=$query&Market=$market";
+		$auth = base64_encode("$acctKey:$acctKey");
+		$data = array(  
+            'http' => array(
+                        'request_fulluri' => true,
+                        'ignore_errors' => true,
+                        'header' => "Authorization: Basic $auth"
+                        )
+            );
+		$context = stream_context_create($data);
+		$response = file_get_contents($requestUri, 0, $context);
+		$response=json_decode($response,true);
+		$collection->insert(array($e["text"]=>$response));
+		for($i=0;$i<5;$i++){
+			echo "<img src='".$response["d"]["results"][$i]["MediaUrl"]."' width='200' height='200'>";
+		}
+		}
+		echo "</br>";
+	}
+	
+	
+	}
+	
+	
+	
 ?>
